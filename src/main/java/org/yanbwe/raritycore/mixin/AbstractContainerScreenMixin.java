053b73ee8aff1872602320141453e12dc72f2954
Yanbwe <GGxiaoyang1@163.com>
2026-03-21 23:43:14 +0800

    修复

--- src/main/java/org/yanbwe/raritycore/mixin/AbstractContainerScreenMixin.java
+++ src/main/java/org/yanbwe/raritycore/mixin/AbstractContainerScreenMixin.java
@@ -3,30 +3,9 @@ package org.yanbwe.raritycore.mixin;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.client.gui.screens.inventory.AbstractContainerScreen;
 import net.minecraft.world.inventory.Slot;
-import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.yanbwe.raritycore.client.ItemBorderRenderer;
 
 @Mixin(AbstractContainerScreen.class)
-public class AbstractContainerScreenMixin {
+public abstract class AbstractContainerScreenMixin {
 
-    // 注入到renderSlot方法中,在物品和装饰渲染完成后添加边框
-    @Inject(
-        method = "renderSlot(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/world/inventory/Slot;)V", 
-        at = @At(
-            value = "INVOKE", 
-            target = "Lnet/minecraft/client/gui/GuiGraphics;renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;IILjava/lang/String;)V",
-            shift = At.Shift.AFTER
-        )
-    )
-    private void renderSlot(GuiGraphics guiGraphics, Slot slot, CallbackInfo ci) {
-        // 在物品渲染后添加稀有度边框
-        ItemStack itemStack = slot.getItem();
-        int x = slot.x;
-        int y = slot.y;
-        ItemBorderRenderer.renderRarityBorder(guiGraphics, itemStack, x, y);
-    }
 }
--- src/main/java/org/yanbwe/raritycore/mixin/GuiMixin.java
+++ src/main/java/org/yanbwe/raritycore/mixin/GuiMixin.java
@@ -1,24 +1,23 @@
 package org.yanbwe.raritycore.mixin;
 
+import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.At.Shift;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.yanbwe.raritycore.client.ItemBorderRenderer;
 
 @Mixin(Gui.class)
-public class GuiMixin
-{
-	@Inject(method = "renderSlot",
-			at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/GuiGraphics;renderItemDecorations(Lnet/minecraft/client/gui/Font;Lnet/minecraft/world/item/ItemStack;II)V", shift = Shift.AFTER))
-	private void renderSlot(GuiGraphics graphics, int x, int y, float time, Player player, ItemStack item, int something, CallbackInfo info)
-	{
-		// 渲染热键栏物品边框 - 使用根据稀有度变化的颜色
-		ItemBorderRenderer.renderRarityBorder(graphics, item, x, y);
-	}
+public class GuiMixin {
+    @Inject(method = "renderSlot(Lnet/minecraft/client/gui/GuiGraphics;IILnet/minecraft/client/DeltaTracker;Lnet/minecraft/world/entity/player/Player;Lnet/minecraft/world/item/ItemStack;I)V",
+            at = @At("TAIL"))
+    private void renderSlotWithRarityBorder(GuiGraphics guiGraphics, int x, int y, DeltaTracker deltaTracker, Player player, ItemStack itemStack, int slotIndex, CallbackInfo ci) {
+        if (!itemStack.isEmpty()) {
+            ItemBorderRenderer.renderRarityBorder(guiGraphics, itemStack, x, y);
+        }
+    }
 }
